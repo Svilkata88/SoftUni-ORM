e8a66b981ce030483d617230b39e4e1ea58ccbb4
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-07-17 21:33:25 +0300

    6th task

--- 6.Exercise Django Models Relations/main_app/admin.py
+++ 6.Exercise Django Models Relations/main_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from main_app.models import Car
+
+
 # Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'year', 'owner', 'car_details')
+
+    @staticmethod
+    def car_details(obj: object) -> str:
+        try:
+            owner_name = obj.owner.name
+        except AttributeError:
+            owner_name = 'No owner'
+
+        try:
+            reg = obj.registration.registration_number
+        except AttributeError:
+            reg = 'No registration number'
+
+        return f'Owner: {owner_name}, Registration: {reg}'
+
+    car_details.short_description = 'Car Details'
