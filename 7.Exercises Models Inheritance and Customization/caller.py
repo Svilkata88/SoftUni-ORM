276235f4fa016a115c2a4eabfde13db6e456756c
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-07-20 18:51:57 +0300

    2nd and 3th task

--- 7.Exercises Models Inheritance and Customization/caller.py
+++ 7.Exercises Models Inheritance and Customization/caller.py
@@ -6,7 +6,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Mage, Necromancer
+
+from main_app.models import Mage, Necromancer, Message, UserProfile, Student
+
+# Task 1 / Character Classes
+
 # Create queries within functions
 # Create instances
 # mage = Mage.objects.create(
@@ -29,3 +33,44 @@ from main_app.models import Mage, Necromancer
 # print(necromancer.name)
 # print(necromancer.description)
 # print(necromancer.raise_dead_ability)
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# Task 2 / Chat App
+
+# user1 = UserProfile.objects.create(username='john_doe', email='john@example.com',bio='Hello, I am John Doe.')
+# user2 = UserProfile.objects.create(username='jane_smith', email='jane@example.com',bio='Hi there, I am Jane Smith.')
+# user3 = UserProfile.objects.create(username='alice', email='alice@example.com',bio='Hello, I am Alice.')
+# # Create a message from user1 to user2
+# message1 = Message.objects.create(sender=user1,receiver=user2,content="Hello, Jane! Could you please tell Alice that tomorrow we are going onvacation?")
+# # Display the content
+# print(message1.content)
+# # Mark the message as read
+# message1.mark_as_read()
+# print(f"Is read: {message1.is_read}")
+# # Create a reply from user2 to user1
+# reply_message = message1.reply_to_message(reply_content="Hi John, sure! I will forward this message to her!")
+# # Display the content
+# print(reply_message.content)
+# # Create a forwarded message from user2 to user3
+# forwarded_message = message1.forward_message(receiver=user3)
+# print(f"Forwarded message from {forwarded_message.sender.username} to{forwarded_message.receiver.username}")
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# Task 3 / Student Information
+
+# # Test cases
+# student1 = Student(name="Alice", student_id=45.23)
+# student1.full_clean()
+# student1.save()
+# retrieved_student1 = Student.objects.get(name="Alice")
+# # Print the saved ID of the student1
+# print(retrieved_student1.student_id)
+# # Try to parse zero as ID and expect ValueError
+# try:
+#     student2 = Student(name="Bob", student_id="0")
+#     student1.full_clean()
+#     student2.save()
+# except ValueError as error:
+#     (print(error))
--- 7.Exercises Models Inheritance and Customization/main_app/models.py
+++ 7.Exercises Models Inheritance and Customization/main_app/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -51,3 +52,58 @@ class VengeanceDemonHunter(DemonHunter):
 class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
 
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self) -> None:
+        self.is_read = True
+        # should be saved - self.save()
+
+    def reply_to_message(self, reply_content: str) -> "Message":
+        message = Message(
+            sender=self.receiver,
+            receiver=self.sender,
+            content=reply_content
+        )
+        message.save()
+        return message
+
+    def forward_message(self, receiver: UserProfile) -> "Message":
+        message = Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=self.content
+        )
+        message.save()
+        return message
+
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value) -> int or None:
+        try:
+            return int(value)
+        except ValueError:
+            raise ValueError('Invalid input for student ID')
+
+    def get_prep_value(self, value) -> int or None:
+        checked_value = self.to_python(value)
+        if checked_value <= 0:
+            raise ValidationError('ID cannot be less than or equal to zero')
+
+        return checked_value
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = StudentIDField()
